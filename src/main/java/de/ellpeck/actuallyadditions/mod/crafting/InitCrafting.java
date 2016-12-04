456d46da195043ba5e8f2b4540a633a7042b301c
Ellpeck <megamaximal@gmail.com>
2016-12-04 15:36:14 +0100

    Farming cactus + nice description

--- src/main/java/de/ellpeck/actuallyadditions/mod/crafting/InitCrafting.java
+++ src/main/java/de/ellpeck/actuallyadditions/mod/crafting/InitCrafting.java
@@ -14,6 +14,7 @@ import de.ellpeck.actuallyadditions.api.ActuallyAdditionsAPI;
 import de.ellpeck.actuallyadditions.mod.fluids.InitFluids;
 import de.ellpeck.actuallyadditions.mod.items.InitItems;
 import de.ellpeck.actuallyadditions.mod.items.metalists.TheMiscItems;
+import de.ellpeck.actuallyadditions.mod.misc.apiimpl.farmer.CactusFarmerBehavior;
 import de.ellpeck.actuallyadditions.mod.misc.apiimpl.farmer.DefaultFarmerBehavior;
 import de.ellpeck.actuallyadditions.mod.util.ModUtil;
 import net.minecraft.init.Blocks;
@@ -40,6 +41,7 @@ public final class InitCrafting{
         ActuallyAdditionsAPI.addOilGenRecipe(InitFluids.fluidEmpoweredOil.getName(), 350);
 
         ActuallyAdditionsAPI.addFarmerBehavior(new DefaultFarmerBehavior());
+        ActuallyAdditionsAPI.addFarmerBehavior(new CactusFarmerBehavior());
 
         RecipeSorter.register(ModUtil.MOD_ID+":recipeKeepDataShaped", RecipeKeepDataShaped.class, RecipeSorter.Category.SHAPED, "after:minecraft:shaped");
         RecipeSorter.register(ModUtil.MOD_ID+":recipeKeepDataShapeless", RecipeKeepDataShapeless.class, RecipeSorter.Category.SHAPELESS, "after:minecraft:shapeless");
